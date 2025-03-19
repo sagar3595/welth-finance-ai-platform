ab811c727686cde8e7ba15390532651f6abaa4e8
sagar3595 <sagar47kumar13@gmail.com>
2025-03-20 00:22:16 +0530

    added no-pagination

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         {/* footer */}
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center">
-            <p>Made with By ❤️ Sagar</p>
+            <p>Made with ❤️ By Sagar</p>
           </div>
         </footer>
       </body>
